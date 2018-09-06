4a4fe7f1869dcdd67b85b3d53d34d800475ce978
John Tillman <john.w.tillman@gmail.com>
2018-09-06 19:48:22 -0400

    connection creds

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,10 @@
 const mysql = require('mysql')
 
-const hconnection = mysql.createConnection({
+const connection = mysql.createConnection({
   host     : 'us-cdbr-iron-east-01.cleardb.net',
   user     : 'ba19c37d58e9eb',
   password : '787b146a',
   database : 'heroku_378b7ee547f02e5',
-  multipleStatements: true
-});
-
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'bootcamp',
-  database : 'burger_db',
   multipleStatements: true
 });
 
